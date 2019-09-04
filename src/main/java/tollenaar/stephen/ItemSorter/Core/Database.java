243004da852054451636f25e7569c172feb2de69
Stephen <1guitargun1@gmail.com>
2019-09-03 23:15:07 -0230

    version tracking and backwards support

--- src/main/java/tollenaar/stephen/ItemSorter/Core/Database.java
+++ src/main/java/tollenaar/stephen/ItemSorter/Core/Database.java
@@ -2,6 +2,7 @@ package tollenaar.stephen.ItemSorter.Core;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -18,7 +19,12 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemFrame;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import tollenaar.stephen.ItemSorter.Util.Book;
 import tollenaar.stephen.ItemSorter.Util.Frame;
 import tollenaar.stephen.ItemSorter.Util.Hopper;
@@ -27,6 +33,7 @@ public class Database {
 
 	private ItemSorter plugin;
 	private Connection connection;
+	private static final String VERSION = "1.1";
 
 	public Database(ItemSorter plugin) {
 		this.plugin = plugin;
@@ -671,9 +678,79 @@ public class Database {
 		}
 	}
 
+	public void loadVersion() {
+		PreparedStatement pst = null;
+		try {
+			pst = getConnection().prepareStatement("SELECT * FROM `Version`;");
+			ResultSet rs = pst.executeQuery();
+			if (!rs.next()) {
+				plugin.getLogger().log(Level.WARNING, "Newly created table, adding values and reloading the loaded configurations. This can tkae a while!!");
+				pst.close();
+				pst = getConnection().prepareStatement("INSERT INTO `Version` (`version`) VALUES (1.0);");
+				pst.execute();
+				load();
+			} else {
+				if (rs.getString("version").equals("1.0")) {
+					plugin.getLogger().log(Level.WARNING, "Old non supported version detected, updating current deployed books. This can take a while!");
+					for(Location loc : Frame.getFrames()){
+						for (Entity ent : loc.getChunk().getEntities()) {
+							// getting the right item frame
+							if (ent instanceof ItemFrame && ent.getLocation().equals(loc)) {
+								ItemFrame fr = (ItemFrame) ent;
+								if(fr.getItem() != null && fr.getItem().getType() == Material.WRITTEN_BOOK){
+									ItemStack item = fr.getItem();
+									BookMeta meta = (BookMeta) item.getItemMeta();
+									
+									if(!meta.getPage(meta.getPageCount()).contains("To edit the configuration click here.")){
+									
+									Book book = (Book) Book.fromString(meta.getLore().get(0).replaceAll("§", ""));
+									
+									BaseComponent[] editPage = new ComponentBuilder("To edit the configuration click here.")
+											.event(new ClickEvent(ClickEvent.Action.OPEN_URL, plugin.getConfig().getString("URL")
+													+ plugin.getConfig().getString("editPageResponse") + "?configData=" + URLEncoder.encode(book.toString(), "UTF-8")))
+											.create();
+
+									meta.spigot().addPage(editPage);
+
+									// changing the item frame item
+									item.setItemMeta(meta);
+									}
+								}
+							}
+						}
+					}
+				}
+				if(!rs.getString("version").equals(VERSION)){
+					plugin.getLogger().log(Level.WARNING, "Updating Version.");
+					String old = rs.getString("version");
+					pst.close();
+					pst = getConnection().prepareStatement("UPDATE `Version` SET `version`=? WHERE `version`=?;");
+					pst.setString(1, VERSION);
+					pst.setString(2, old);
+					pst.execute();
+				}
+			}
+
+		} catch (SQLException | ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pst != null) {
+					pst.close();
+				}
+			} catch (SQLException ex) {
+				System.out.println(ex.getStackTrace());
+			}
+		}
+	}
+
 	public void load() {
+		plugin.getLogger().log(Level.INFO, "Loading Frames.");
 		loadFrames();
+		plugin.getLogger().log(Level.INFO, "Loading Hoppers.");
 		loadHoppers();
+		plugin.getLogger().log(Level.INFO, "Checking Version.");
+		loadVersion();
 	}
 
 	public void initialize() {
@@ -692,7 +769,8 @@ public class Database {
 					+ "(userUUID TEXT NOT NULL, frame_id INTEGER UNIQUE NOT NULL, "
 					+ "CONSTRAINT fk_Frames FOREIGN KEY (frame_id) REFERENCES Frames(id) ON DELETE CASCADE); PRAGMA foreign_keys=ON;"
 					+ "CREATE TABLE IF NOT EXISTS EditUserConfigs "
-					+ "(userUUID TEXT NOT NULL, bookValue TEXT PRIMARY KEY);");
+					+ "(userUUID TEXT NOT NULL, bookValue TEXT PRIMARY KEY);"
+					+ "CREATE TABLE IF NOT EXISTS Version (version TEXT NOT NULL);");
 			statement.close();
 		} catch (SQLException e) {
 			plugin.getLogger().log(Level.SEVERE, e.getMessage());
