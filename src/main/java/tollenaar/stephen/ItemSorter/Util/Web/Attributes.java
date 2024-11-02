28f9fe26e5cfa151e0bb9821a1310aebc7edce51
Stephen Tollenaar <stephen@tollenaar.com>
2024-11-02 18:42:15 -0230

    sorted items by name and added selectedonly filter

--- src/main/java/tollenaar/stephen/ItemSorter/Util/Web/Attributes.java
+++ src/main/java/tollenaar/stephen/ItemSorter/Util/Web/Attributes.java
@@ -1,6 +1,7 @@
 package tollenaar.stephen.ItemSorter.Util.Web;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.lang3.text.WordUtils;
@@ -9,42 +10,45 @@ import org.bukkit.potion.PotionEffectType;
 
 public class Attributes {
 
-	private final List<Item> items;
-	private final List<Image> containers;
-	private final List<Item> enchantments;
-	private final List<Item> potions;
+    private final List<Item> items;
+    private final List<Image> containers;
+    private final List<Item> enchantments;
+    private final List<Item> potions;
 
-	public Attributes(List<Item> items, List<Image> containers) {
-		this.items = items;
-		this.containers = containers;
-		
-		this.enchantments = new ArrayList<>();
-		for (Enchantment ent : Enchantment.values()) {
-			enchantments.add(new Item(ent.hashCode(), 1, ent.getKey().getKey(),
-					WordUtils.capitalizeFully(ent.getKey().getKey().replace("_", " "))));
-		}
-	
+    public Attributes(List<Item> items, List<Image> containers) {
+        Collections.sort(items, (a, b) -> {
+            return a.getName().compareTo(b.getName());
+        });
+        this.items = items;
+        this.containers = containers;
 
-		this.potions = new ArrayList<>();
-		for (PotionEffectType pot : PotionEffectType.values()) {
-			potions.add(new Item(pot.hashCode(), 1, pot.getName(), WordUtils.capitalizeFully(pot.getName().replace("_"," " ))));
-		}
-	}
+        this.enchantments = new ArrayList<>();
+        for (Enchantment ent : Enchantment.values()) {
+            enchantments.add(new Item(ent.hashCode(), 1, ent.getKey().getKey(),
+                    WordUtils.capitalizeFully(ent.getKey().getKey().replace("_", " "))));
+        }
 
-	public List<Item> getItems() {
-		return items;
-	}
+        this.potions = new ArrayList<>();
+        for (PotionEffectType pot : PotionEffectType.values()) {
+            potions.add(new Item(pot.hashCode(), 1, pot.getName(),
+                    WordUtils.capitalizeFully(pot.getName().replace("_", " "))));
+        }
+    }
 
-	public List<Image> getContainers() {
-		return containers;
-	}
+    public List<Item> getItems() {
+        return items;
+    }
 
-	public List<Item> getEnchantments() {
-		return enchantments;
-	}
+    public List<Image> getContainers() {
+        return containers;
+    }
 
-	public List<Item> getPotions() {
-		return potions;
-	}
+    public List<Item> getEnchantments() {
+        return enchantments;
+    }
+
+    public List<Item> getPotions() {
+        return potions;
+    }
 
 }
--- src/main/resources/web/js/outputSelector.js
+++ src/main/resources/web/js/outputSelector.js
@@ -1,77 +1,88 @@
-    let sprites = [];
+let sprites = [];
 
 
-    function drawBoard(canvas) {
-        //grid square width
-        let context = canvas.getContext("2d");
-        const cw = 32;	//cell width
-        const ch = 32;	//cell height
-        const bw = canvas.width;
-        const bh = canvas.height;
-    
-    
-        for (let x = 0; x <= bw; x += cw) {
-            context.moveTo(0.5 + x, 0);
-            context.lineTo(0.5 + x, bh);
-        }
-    
-        //grid square height
-        for (let x = 0; x <= bh; x += ch) {
-            context.moveTo(0, 0.5 + x);
-            context.lineTo(bw, 0.5 + x);
-        }
-        context.strokeStyle = "black";
-        context.stroke();
+function drawBoard(canvas) {
+    //grid square width
+    let context = canvas.getContext("2d");
+    const cw = 32;	//cell width
+    const ch = 32;	//cell height
+    const bw = canvas.width;
+    const bh = canvas.height;
+
+
+    for (let x = 0; x <= bw; x += cw) {
+        context.moveTo(0.5 + x, 0);
+        context.lineTo(0.5 + x, bh);
     }
 
-    //so this is going to dynamically add the tiles to the table
-    function loadItems() {
-        //this is the table we are using
-        const table = document.getElementById("items").children[0];
-        table.removeChild(table.children[0]);
-        table.appendChild(document.createElement("tbody"));
-        let row = table.insertRow(0);
-        let column = 0;
-        const c = row.insertCell(column);
+    //grid square height
+    for (let x = 0; x <= bh; x += ch) {
+        context.moveTo(0, 0.5 + x);
+        context.lineTo(bw, 0.5 + x);
+    }
+    context.strokeStyle = "black";
+    context.stroke();
+}
+
+function filterSprites(state) {
+    const ul = document.getElementById("searchUL");
+    for (const item of ul.children) {
+        if (item.children[0].className.includes("active") != state.checked) {
+            item.style.display = 'none'
+        } else {
+            item.style.display = ''
+        }
+    }
+}
+
+//so this is going to dynamically add the tiles to the table
+function loadItems() {
+    //this is the table we are using
+    const table = document.getElementById("items").children[0];
+    table.removeChild(table.children[0]);
+    table.appendChild(document.createElement("tbody"));
+    let row = table.insertRow(0);
+    let column = 0;
+    const c = row.insertCell(column);
+    const smallC = document.createElement("canvas");
+    smallC.width = 32;
+    smallC.height = 32;
+    c.setAttribute("id", 1000);
+    c.onclick = function () { //insert whatever function handled the tiles selection here}
+        editor.selection = parseInt(this.id);
+    }
+    c.appendChild(smallC);
+    column++;
+    for (let sprite of sprites) {
+
+        //adding a new row
+        if (column == 0) {
+            row = table.insertRow(table.rows.length);
+        }
+        //adding a new cell at the index of the column
+        const cell = row.insertCell(column);
+        cell.setAttribute("id", sprite);
+        cell.onclick = function () { //insert whatever function handled the tiles selection here}
+            editor.selection = sprite;
+        }
+        //setting the image, we need to create different icons for every image it seems, every cell has his own id as well to use with the function,
+        //this should all work
         const smallC = document.createElement("canvas");
         smallC.width = 32;
         smallC.height = 32;
-        c.setAttribute("id", 1000);
-        c.onclick = function() { //insert whatever function handled the tiles selection here}
-            editor.selection = parseInt(this.id);
-        }
-        c.appendChild(smallC);
+        smallC.getContext('2d').drawImage(
+            document.getElementById(`IM${sprite}`), //the image
+            0, //start x of the image in the spritesheet
+            0, //start y of the image in the spritesheet
+            16, //width of the sprite
+            16, //height of the sprite
+            0, //destination x of the sprite on the canvas
+            0, //destination of y of the sprite on the canvas
+            32, //width of sprite
+            32 //height of sprite
+        );
+        cell.appendChild(smallC);
         column++;
-        for (let sprite of sprites) {
-
-            //adding a new row
-            if (column == 0) {
-                row = table.insertRow(table.rows.length);
-            }
-            //adding a new cell at the index of the column
-            const cell = row.insertCell(column);
-            cell.setAttribute("id", sprite);
-            cell.onclick = function() { //insert whatever function handled the tiles selection here}
-                    editor.selection = sprite;
-                }
-                //setting the image, we need to create different icons for every image it seems, every cell has his own id as well to use with the function,
-                //this should all work
-            const smallC = document.createElement("canvas");
-            smallC.width = 32;
-            smallC.height = 32;
-            smallC.getContext('2d').drawImage(
-                document.getElementById(`IM${sprite}`), //the image
-                0, //start x of the image in the spritesheet
-                0, //start y of the image in the spritesheet
-                16, //width of the sprite
-                16, //height of the sprite
-                0, //destination x of the sprite on the canvas
-                0, //destination of y of the sprite on the canvas
-                32, //width of sprite
-                32 //height of sprite
-            );
-            cell.appendChild(smallC);
-            column++;
-            if (column == 3) { column = 0; }
-        }
+        if (column == 3) { column = 0; }
     }
+}
